561249e93ac08ef170091c736d5d0dab0ff54ddf
lexy <lexyblazy@gmail.com>
2017-08-29 18:00:52 +0100

    created post model

--- models/post.js
+++ models/post.js
@@ -1,1 +1,22 @@
-const mongoose = require('')
+const mongoose = require('mongoose');
+// const validator = require('validator')
+mongoose.Promise = global.Promise;
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:'Please supply a post title',
+        trim:true
+    },
+    content:{
+        type:String,
+        required:'Post must have a content'
+    },
+    image:String,
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Category'
+    }
+})
+
+module.exports = mongoose.model('Post',postSchema);
